06278b0379d53c93b89886044fe2c74787b20ba2
Jay Pandya <jaypandya2k5@gmail.com>
2022-08-01 13:43:45 -0400

    add video hero

--- cms/lists/hero.ts
+++ cms/lists/hero.ts
@@ -1,7 +1,7 @@
 import { list } from "@keystone-6/core";
 import { cloudinaryImage } from "@keystone-6/cloudinary";
 import dotenv from "dotenv";
-import { text } from "@keystone-6/core/fields";
+import { select, text } from "@keystone-6/core/fields";
 
 dotenv.config();
 
@@ -26,6 +26,24 @@ export const Hero = list({
         apiKey: process.env.CLOUDINARY_API_KEY ?? "",
         apiSecret: process.env.CLOUDINARY_API_SECRET ?? "",
         folder: process.env.CLOUDINARY_API_FOLDER,
+      },
+    }),
+    vidLight: text(),
+    vidDark: text(),
+    backgroundType: select({
+      options: [
+        {
+          label: "Video",
+          value: "video",
+        },
+        {
+          label: "Image",
+          value: "image",
+        },
+      ],
+      defaultValue: "image",
+      ui: {
+        displayMode: "segmented-control",
       },
     }),
   },
